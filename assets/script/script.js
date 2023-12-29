75a72e2712dbcd2cb791c449fbc1b02b49b9f4dc
TrumanStephenson <stephensontruman96@yahoo.com>
2023-12-29 11:58:46 -0800

    Trying to merge a hamburger menu as well

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,11 +1,21 @@
-const button = document.querySelector('.button btn5');
+// const button = document.querySelector('.button btn5');
 
-if(button) {
-    button.parentElement.setAttribute('data-interactive', '');
-    button.removeAttribute('hidden');
-    button.addEventListener('click', evt => {
-        evt.preventDefault();
+// if(button) {
+//     button.parentElement.setAttribute('data-interactive', '');
+//     button.removeAttribute('hidden');
+//     button.addEventListener('click', evt => {
+//         evt.preventDefault();
 
-        alert('Oh hi there 👋');
+//         alert('Oh hi there 👋');
+//     });
+// }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const mobileMenu = document.getElementById('mobile-menu');
+    const navList = document.getElementById('nav-list');
+
+    mobileMenu.addEventListener('click', function () {
+        navList.classList.toggle('show');
     });
-}
+});
