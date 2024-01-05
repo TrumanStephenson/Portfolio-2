d36e0881b32448f7738729bb60b720ef5441b30c
TrumanStephenson <stephensontruman96@yahoo.com>
2024-01-05 10:17:32 -0800

    Saving progress for now. The @media, is not cooperating with the hamburger menu itself and I had to change it from none - block so it actually displayed. But now it just sits dead center. Progress is progress.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,35 +1,12 @@
-// const button = document.querySelector('.button btn5');
-
-// if(button) {
-//     button.parentElement.setAttribute('data-interactive', '');
-//     button.removeAttribute('hidden');
-//     button.addEventListener('click', evt => {
-//         evt.preventDefault();
-
-//         alert('Oh hi there 👋');
-//     });
-// }
-
-//          Commented out to get this nav bar setup
-// document.addEventListener('DOMContentLoaded', function () {
-//     const mobileMenu = document.getElementById('mobile-menu');
-//     const navList = document.getElementById('navigator');
-
-//     mobileMenu.addEventListener('click', function () {
-//         navList.classList.toggle('show');
-//     });
-// });
-
-
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-})
+});
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
-}))
+}));
