ad78486115c5c049f2000d6533b8305ba5174c45
TrumanStephenson <stephensontruman96@yahoo.com>
2024-01-15 01:57:26 -0800

    trying something different with the header. Trying to make the words slowly collapse while it scrolls u, in parabola shape, and then disappear behind the header, even though there is a noticable gap for the top property in header class. But it is late so going to bed.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
+
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
@@ -10,3 +11,8 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }));
+
+window.scrollTo({
+    top: 0;
+    behavior: 'smooth';
+});
