1544c8206836b695017f69c92d7af8191893308d
TrumanStephenson <stephensontruman96@yahoo.com>
2024-01-15 19:40:20 -0800

    Just trying to get this hamburger menu off with some leftover time today. Trying this all over again with just header and menu in a test enviroment with no other assets.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,6 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
-
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
@@ -11,8 +10,3 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }));
-
-window.scrollTo({
-    top: 0;
-    behavior: 'smooth';
-});
